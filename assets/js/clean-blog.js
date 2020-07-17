7dca64347196347ebaec77a920b36e5a20615c41
Flaviu Cojocariu <cojokka@gmail.com>
2020-07-17 13:18:54 +0200

    added pagination; added some minification of js/css/html

--- assets/js/clean-blog.js
+++ assets/js/clean-blog.js
@@ -1,13 +1,31 @@
-(function($) {
+(function() {
   "use strict"; // Start of use strict
 
-  // Floating label headings for the contact form
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('pre code').forEach((block) => {
+      hljs.configure({
+        languages: ['css', 'html', 'javascript', 'python']
+      });
+      hljs.highlightBlock(block);
+    });
   });
 
-})(jQuery); // End of use strict
+  var formLabelGroup = document.querySelectorAll('.floating-label-form-group');
+
+  for (var i = 0; i < formLabelGroup.length; i++) {
+    formLabelGroup[i].addEventListener('input', function(e) {
+      if(e.target.value.length > 0) {
+        this.classList.add('floating-label-form-group-with-value');
+      } else {
+        this.classList.remove('floating-label-form-group-with-value');
+      }
+    }, true);
+    formLabelGroup[i].addEventListener('focus', function(e) {
+      this.classList.add('floating-label-form-group-with-focus');
+    }, true);
+    formLabelGroup[i].addEventListener('blur', function() {
+      this.classList.remove('floating-label-form-group-with-focus');
+    }, true);
+  }
+
+})(); // End of use strict
--- ssg.py
+++ ssg.py
@@ -2,22 +2,27 @@ import os
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
 from markdown2 import markdown
+from css_html_js_minify import process_single_js_file, process_single_css_file, process_single_html_file
+from jsmin import jsmin
 
-
-POSTS = {}
+POSTS = { }
 
 for markdown_post in os.listdir('content/articles'):
-    file_path = os.path.join('content/articles', markdown_post)
+  file_path = os.path.join('content/articles', markdown_post)
 
-    with open(file_path, 'r') as file:
-        POSTS[markdown_post] = markdown(file.read(), extras=['metadata', 'fenced-code-blocks', 'code-friendly', 'cuddled-lists'])
-        
+  with open(file_path, 'r') as file:
+    POSTS[markdown_post] = markdown(file.read(),
+                                    extras = ['metadata', 'fenced-code-blocks', 'code-friendly', 'cuddled-lists'])
 
-POSTS = {
-    post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%d.%m.%Y'), reverse=True)
-}
+# POSTS = {
+#   post: POSTS[post] for post in sorted(POSTS, key = lambda post: datetime.strptime(POSTS[post].metadata['date'],
+#                                                                                    '%d.%m.%Y'), reverse = True)
+# }
 
-env = Environment(loader=FileSystemLoader('templates'))
+for post in POSTS:
+  print(POSTS[post].metadata)
+
+env = Environment(loader = FileSystemLoader('templates'))
 home_template = env.get_template('home.html')
 post_template = env.get_template('article.html')
 contact_template = env.get_template('contact.html')
@@ -28,16 +33,26 @@ today = (datetime.today()).year
 
 # home convertion
 posts_metadata = [POSTS[post].metadata for post in POSTS]
+tmp_posts_metadata = posts_metadata
 
-home_html = home_template.render(posts=posts_metadata, today=today)
+chunk = 5
+page = 1
 
-with open('output/home.html', 'w') as file:
+for i in range(len(tmp_posts_metadata)):
+  if i % chunk == 0 and i != 0:
+    page += 1
+    chunk += 5
+  posts_metadata[i]['page'] = page
+
+home_html = home_template.render(posts = posts_metadata, today = today)
+
+with open('output/home.html', 'w+') as file:
   file.write(home_html)
 
-#404 convertion
+# 404 convertion
 html_404 = template_404.render()
 
-with open('output/404.html' ,'w') as file:
+with open('output/404.html', 'w+') as file:
   file.write(html_404)
 
 # Posts convertion
@@ -51,25 +66,42 @@ for post in POSTS:
     'date': post_metadata['date']
   }
 
-  post_html = post_template.render(post=post_data)
-  post_file_path = 'output/posts/{slug}.html'.format(slug=post_metadata['slug'], today=today)
+  post_html = post_template.render(post = post_data)
+  post_file_path = 'output/posts/{slug}.html'.format(slug = post_metadata['slug'], today = today)
 
-  os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
-  with open(post_file_path, 'w') as file:
+  os.makedirs(os.path.dirname(post_file_path), exist_ok = True)
+  with open(post_file_path, 'w+') as file:
     file.write(post_html)
-    
+
 # contact convertion    
 with open('content/contact.md', 'r') as contact:
-  contact_data = markdown(contact.read(), extras=['metadata'])
-  contact_html = contact_template.render(content=contact_data)
+  contact_data = markdown(contact.read(), extras = ['metadata'])
+  contact_html = contact_template.render(content = contact_data)
   contact_file_path = 'output/contact.html'
-  with open(contact_file_path, 'w') as file:
+  with open(contact_file_path, 'w+') as file:
     file.write(contact_html)
 
 # about convertion
 with open('content/about.md', 'r') as about:
-  about_data = markdown(about.read(), extras=['metadata'])
-  about_html = about_template.render(content=about_data)
+  about_data = markdown(about.read(), extras = ['metadata'])
+  about_html = about_template.render(content = about_data)
   about_file_path = 'output/about.html'
-  with open(about_file_path, 'w') as file:
+  with open(about_file_path, 'w+') as file:
     file.write(about_html)
+
+# minify the js and css
+process_single_css_file('./assets/css/clean-blog.css', overwrite = False)
+# process_single_js_file('./assets/js/home.js', overwrite = False)
+# process_single_js_file('./assets/js/clean-blog.js', overwrite = False)
+
+with open('./assets/js/home.js', 'rw+') as file:
+  minified = jsmin(file)
+  file.write(minified)
+
+# testing minify html
+for html in os.listdir('./output/'):
+  if os.path.isdir('./output/{}'.format(html)):
+    for html_2 in os.listdir('./output/{}'.format(html)):
+      process_single_html_file('./output/{}/{}'.format(html, html_2), overwrite = True)
+  else:
+    process_single_html_file('./output/{}'.format(html), overwrite = True)
